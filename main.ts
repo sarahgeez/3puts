888b38e48b385878061b0cf4b5c19f5256a24009
sarahgeez <74019269+sarahgeez@users.noreply.github.com>
2021-01-20 16:00:07 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,34 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        . # . # .
+        `)
+})
+input.onSound(DetectedSound.Loud, function () {
+    basic.showIcon(IconNames.Sad)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("ABBA is A+")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . # . . .
+        . # . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        `)
+})
+input.onGesture(Gesture.Shake, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showString("Hello!")
+        basic.clearScreen()
+        basic.showIcon(IconNames.Heart)
+    }
+})
 basic.forever(function () {
 	
 })
